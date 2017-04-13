8218060ba2f45f0dbf1fe12c5c8b810ef0996323
John <yjing@csumb.edu>
2017-04-13 18:26:45 +0000

    description page modified

--- description.php
+++ description.php
@@ -1,3 +1,98 @@
+<?php
+
+    session_start();
+    function getDBConnection($dbname) {
+    $host = "localhost";
+    //$dbname = "tech_checkout";
+    $username = "web_user";
+    $password = "s3cr3t";
+    
+    try {
+        //Creating database connection
+        $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        // Setting Errorhandling to Exception
+        $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+    }
+    catch (PDOException $e) {
+        
+        echo "<span class='errorMSG'>There was some problem connecting to the database! Error: $e</span>";
+        exit();
+        
+    }
+    
+    return $dbConn;
+    
+}
+
+    $conn = getDBConnection("pet_store");
+    $dogSql = "SELECT * from dog where dogid = " . $_GET['dogid'];
+    $catSql = "SELECT * from cat where catid = " . $_GET['catid'];
+    $fishSql = "SELECT * from fish where fishid = " . $_GET['fishid'];
+
+    echo  $_GET['dogid'];
+    if (isset($_GET['dogid']))
+    {
+        $statement = $conn->prepare($dogSql);
+        $statement->execute();
+        $dogs = $statement->fetch(PDO::FETCH_ASSOC); //dogs
+        
+        echo "<br><br><br><br><div id='wrapper'>";
+        echo "<table class= 'speciesTable'>";
+        echo "<tr>";
+        echo "<th>Name</th>";
+        echo "<th>Breed</th>";
+        echo "<th>Gender</th>";
+        echo "<th>Age (Years)</th>";
+        echo "<th>Color</th>";
+        echo "<th> Description</th>";
+        echo "</tr>";
+        
+        echo "<td>".$dogs['name']."</td> <td>".$dogs['breed']."</td> <td>".$dogs['gender']."</td> <td>".$dogs[age]."</td> <td>".$dogs['color']."</td> <td>".$dogs['description'];
+        echo "</div>";
+    }
+    else if (isset($_GET['catid']))
+    {
+        $statement = $conn->prepare($catSql);
+        $statement->execute();
+        $cats = $statement->fetch(PDO::FETCH_ASSOC); // cats
+        
+        echo "<br><br><br><br><div id='wrapper'>";
+        echo "<table class= 'speciesTable'>";
+        echo "<tr>";
+        echo "<th>Name</th>";
+        echo "<th>Breed</th>";
+        echo "<th>Gender</th>";
+        echo "<th>Age (Years)</th>";
+        echo "<th>Color</th>";
+        echo "<th> Description</th>";
+        echo "</tr>";
+        
+        echo "<td>".$cats['name']."</td> <td>".$cats['breed']."</td> <td>".$cats['gender']."</td> <td>".$cats[age]."</td> <td>".$cats['color']."</td> <td>".$cats['description'];
+        echo "</div>";
+    }
+    else if (isset($_GET['fishid']))
+    {
+        $statement = $conn->prepare($fishSql);
+        $statement->execute();
+        $fishs = $statement->fetch(PDO::FETCH_ASSOC); // fish
+        
+        echo "<br><br><br><br><div id='wrapper'>";
+        echo "<table class= 'speciesTable'>";
+        echo "<tr>";
+
+        echo "<th>Breed</th>";
+        echo "<th>Gender</th>";
+        echo "<th>Color</th>";
+        echo "<th> Description</th>";
+        echo "</tr>";
+        
+        echo "<td>".$fishs['breed']."</td> <td>".$fishs['gender']."</td> <td>".$fishs['color']."</td> <td>".$fishs['description'];
+        echo "</div>";
+    }
+    
+    
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
--- main.php
+++ main.php
@@ -178,7 +178,7 @@ function printOut($animal)
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
      
-    <style> @import url("styles.css"); </style>
+    <style> @import url("styles.css") </style>
     
     <body>
         <div id= "wrapper">
