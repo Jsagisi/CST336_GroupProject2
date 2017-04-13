d5da1251ebd41aa4c576e0df674fbad066b163fc
John <yjing@csumb.edu>
2017-04-13 16:40:46 +0000

    add description

---
+++ description.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Description </title>
+        <style>
+            @import url("styles.css")
+        </style>
+    </head>
+    <body>
+
+    </body>
+</html>
